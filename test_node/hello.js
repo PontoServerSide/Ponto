547746cb9060270669cf2dbeb9590d6e8e8f14ff
hanmilu <luis.hanmilu@gmail.com>
2014-08-08 14:45:43 +0900

    asdffdsadsaf

--- test_node/hello.js
+++ test_node/hello.js
@@ -8,7 +8,7 @@ var app = express();
 app.use(logfmt.requestLogger());
 
 app.get('/',function(req, res) {
-    res.send('Hello, World!' + process.env + "LAST");
+    res.send('Hello, World!' + process.env + "LAST2");
 });
 
 var port = Number(process.env.PORT || 4000);
