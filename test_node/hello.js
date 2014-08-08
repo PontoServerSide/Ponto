1fe6d0fd4043ba5e0a570908a9c5ca546a5365dc
hanmilu <luis.hanmilu@gmail.com>
2014-08-08 14:09:16 +0900

    eeeeeeer

--- test_node/hello.js
+++ test_node/hello.js
@@ -3,31 +3,19 @@
  */
 var express = require('express');
 var logfmt = require('logfmt');
-var os=require('os');
 var app = express();
 
 app.use(logfmt.requestLogger());
 
 app.get('/',function(req, res) {
-
-    var ifaces=os.networkInterfaces();
-    for (var dev in ifaces) {
-        var alias=0;
-        ifaces[dev].forEach(function(details){
-            if (details.family=='IPv4') {
-                console.log(dev + (alias ? ':' + alias : ''), details.address);
-                res.send('Hello, World!' + app.env.PORT + dev + (alias ? ':' + alias : ''), details.address);
-                ++alias;
-                break;
-            }
-        });
-    }
+    res.send('Hello, World!' + process.env);
 });
 
 var port = Number(process.env.PORT || 4000);
 app.listen(port,function() {
     console.log('Listening on '+port);
 });
+
 /*
 Test3
 */
